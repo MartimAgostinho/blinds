1bcc5008a020cc3345c80cc3a6099ec801e5134a
MartimAgostinho <martim.d.agostinho@protonmail.com>
2022-07-17 02:30:53 +0100

    make blind and gpio

--- blinds.c
+++ blinds.c
@@ -1,4 +1,6 @@
+#include <pigpio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
 
@@ -7,96 +9,138 @@
 //NEEDS SUDO PERMISSION
 void strapp(char *str,...);
 void wait(int seconds);
+void intcpy( int * dest, int * src );
 
-void set_blind_gpio(/*args*/){
-    //Abrir estores pelo GPIO do raspberry pi
+void intcpy( int * dest, int * src ){
+
+    for(int i = 0;i < src[0];++i){ dest[0] = src[0]; }
+
+}
+
+blind make_blind(char * room_name,char * ID,int * open_hours,int * close_hours,unsigned int port_open,unsigned int port_close ){
+
+    blind b;
+    
+    b.room_name = (char *)malloc( sizeof(char) * ( strlen(room_name + 1) ) );
+    if( b.room_name == NULL ){ exit(0); }
+    strcpy(b.room_name, room_name);
+
+    if( !ID ){ b.ID = 0; }
+    else{
+        b.ID = (char *)malloc( sizeof(char) * ( strlen(ID) + 1));
+        if( b.ID == NULL ){ exit(0); }
+        strcpy(b.ID, ID);
+    }
+    
+    b.open_hours = (int *)malloc( sizeof(int) * (open_hours[0]) );
+    if(b.open_hours == NULL){ exit(0); }
+    intcpy(b.open_hours, open_hours);
+
+    b.close_hours = (int *)malloc( sizeof(int) * (close_hours[0]) );
+    if(b.close_hours == NULL){ exit(0); }
+    intcpy(b.close_hours, close_hours);
+
+    b.port_open  = port_open;
+    b.port_close = port_close;
+
+    return b;
+}
+
+void free_blind(blind b){
+    free(b.room_name);
+    free(b.ID);
+    free(b.open_hours);
+    free(b.close_hours);
+}
+
+void set_blind_gpio(unsigned int ID,int set){
+    
+    int log = gpioWrite(ID, set);
+
+    if( !log ){
+        //TODO: save to a file
+        if( log == PI_BAD_GPIO ){ printf(" BAD GPIO\n "); }
+        else if( log == PI_BAD_LEVEL ){ printf(" BAD LEVEL\n "); }
+        exit(0);
+    }
+
 }
 
 void set_blind_relay( int relay,char * id,char set ){
 
-    char * cmd = (char *)malloc( sizeof(char) * ( strlen(id) + 14 ) );//14 = number of char in usbrelay ID_n=s
+    char * cmd = (char *)malloc( sizeof(char) * ( strlen(id) + 14 ) );//14 = number of char in "usbrelay ID_n=s"
+    
+    if( cmd == NULL ) { 
+        free(cmd);
+        exit(0);
+    }
+    
     char rel[2],st[2];
     
-    rel[0] = relay +'0';
+    rel[0] = relay +'0';//conversion to char
     rel[1] = '\0';
-    st[0] = set +'0';
-    st[1] = '\0';
+    st [0] = set +'0';
+    st [1] = '\0';
 
     cmd[0] = '\0';
     //build shell comand
     strapp(cmd,"usbrelay ",id,"_",rel,"=",st,0);    
-    system(cmd);
-}
-
-void open_blind_gpio(blind b){
-    //Abrir estores pelo GPIO do raspberry pi
-}
-
-void open_blind_relay(blind b){
-    set_blind_relay(b.port_open, b.ID ,1);
-}
-
-void close_blind_gpio(blind b){
-    //Abrir estores pelo GPIO do raspberry pi
-}
-
-void close_blind_relay(blind b){
-    set_blind_relay(b.port_close, b.ID ,1);
-}
-
-void stop_blind_gpio(blind b){
-
-}
-
-void stop_blind_relay(blind b){
-    set_blind_relay(b.port_close, b.ID ,0);
-    set_blind_relay(b.port_open, b.ID ,0);
-}
-
-void /*char*/ open_blind(blind b){
+    int log = system(cmd);
     
+    if( !log ){//TODO: save log to file
+        free(cmd);
+        exit(0);
+    }
 
-    if( !b.ID ){ 
-        open_blind_gpio(b);
+    free(cmd);
+}
+
+void  open_blind(blind b){
+
+    if( !b.ID ){//GPIO 
+        set_blind_gpio(b.port_open,1);
         wait(BLIND_TIME);
-        stop_blind_gpio(b);
+        set_blind_gpio(b.port_open,0);
     }  
 
     else{ 
-        open_blind_relay(b);
+        set_blind_relay(b.port_open,b.ID,1);
         wait(BLIND_TIME);
-        stop_blind_gpio(b);
+        set_blind_relay(b.port_open,b.ID,0);
     }
-
-    //return res;
 }
 
-void /*char*/ open_blind_gen( void ** args ){
+void close_blind(blind b){
+
+    if( !b.ID ){ 
+        set_blind_gpio(b.port_close,1);
+        wait(BLIND_TIME);
+        set_blind_gpio(b.port_close,0);
+    }  
+
+    else{ 
+        set_blind_relay(b.port_close,b.ID,1);
+        wait(BLIND_TIME);
+        set_blind_relay(b.port_close,b.ID,0);
+    }
+}
+
+void open_blind_gen( void ** args ){
     
     if( !args[1] ){ return; }//args have to end with value 0( prevent coredump )
 
     blind *b = args[0];
 
     open_blind( *b );
-
 }
 
-void /*char*/ close_blind(blind b){
+void close_blind_gen( void ** args ){
     
+    if( !args[1] ){ return; }//args have to end with value 0( prevent coredump )
 
-    if( !b.ID ){ 
-        close_blind_gpio(b);
-        wait(BLIND_TIME);
-        stop_blind_gpio(b);
-    }  
+    blind *b = args[0];
 
-    else{ 
-        close_blind_relay(b);
-        wait(BLIND_TIME);
-        stop_blind_gpio(b);
-    }
-
-    //return res;
+    close_blind( *b );
 }
 
 void wait(int seconds){
@@ -111,7 +155,6 @@ void strapp(char *str,...){
     va_start(args,str);
     
     char * str2 = va_arg(args, char *);
-
 
     while( str2 != 0 ){
 
--- blinds.h
+++ blinds.h
@@ -1,20 +1,27 @@
 #ifndef _BLIND_H_
 #define _BLIND_H_
 
+#include <pigpio.h>
+
 #define GPIO "gpio"
 #define BLIND_TIME 100 //time in seconds
 
 typedef struct {
 
-    char *  room_name;        //string with room name
-    char *  ID;              //0 if raspberry, relay its id ex.:("/dev/hidraw10") 
-    int  *  open_hours;     //list with opening hours in minutes, since midnight
-    int  *  close_hours;   //==
-    int     port_open;    //if raspberry pi,its gpio port, if relay, its number
-    int     port_close;  //==
+    char    *    room_name;        //string with room name
+    char    *    ID;              //0 if raspberry, relay its id ex.:("/dev/hidraw10") 
+    int     *    open_hours;     //First number is lenght, list with opening hours in minutes, since midnight
+    int     *    close_hours;   //==
+    unsigned int port_open;    //if raspberry pi,its gpio port, if relay, its number
+    unsigned int port_close;  //==
 
 }blind;
 
+blind make_blind(char * room_name,char * ID,int * open_hours,int * close_hours,unsigned int port_open,unsigned int port_close );
+void free_blind(blind b);
 void open_blind(blind b);
+void close_blind(blind b);
+void open_blind_gen(void ** args);
+void close_blind_gen(void ** args);
 
 #endif
